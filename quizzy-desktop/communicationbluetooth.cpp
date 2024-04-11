445c80a5c2e87ac44b91771a3d28d4001e439cc4
ThomasHni <thomas.hnizdo@gmail.com>
2024-04-11 16:09:17 +0200

    QUIZ24-17 Codage de la connexion Bluetooth

--- quizzy-desktop/communicationbluetooth.cpp
+++ quizzy-desktop/communicationbluetooth.cpp
@@ -1,13 +1,87 @@
+/**
+ * @file communicationbluetooth.cpp
+ *
+ * @brief Définition de la classe CommunicationBluetooth
+ * @author Thomas HNIZDO
+ * @version 0.2
+ */
 #include "communicationbluetooth.h"
 #include <QDebug>
 
+/**
+ * @brief Constructeur de la classe CommunicationBluetooth
+ */
 CommunicationBluetooth::CommunicationBluetooth(QObject* parent) :
     QObject(parent)
 {
     qDebug() << Q_FUNC_INFO;
+    verifierBluetooth();
 }
 
+/**
+ * @brief Destructeur de la classe Participant
+ */
 CommunicationBluetooth::~CommunicationBluetooth()
 {
     qDebug() << Q_FUNC_INFO;
+}
+
+void CommunicationBluetooth::verifierBluetooth()
+{
+    if(appareilLocal.isValid())
+    {
+        qDebug() << "Bluetooth disponible";
+        activerBluetooth();
+        lireNomAppareil();
+        rendreAppareilVisible();
+        obtenirAppareilsConnectes();
+    }
+    else
+    {
+        qDebug() << "Bluetooth n'est pas disponible.";
+    }
+}
+
+void CommunicationBluetooth::activerBluetooth()
+{
+    appareilLocal.powerOn();
+    qDebug() << "Bluetooth activé.";
+}
+
+void CommunicationBluetooth::lireNomAppareil()
+{
+    nomAppareilLocal = appareilLocal.name();
+    qDebug() << "nom de l'appareil :" << nomAppareilLocal;
+}
+
+void CommunicationBluetooth::rendreAppareilVisible()
+{
+    appareilLocal.setHostMode(QBluetoothLocalDevice::HostDiscoverable);
+    qDebug() << "Appareil visible";
+}
+
+void CommunicationBluetooth::obtenirAppareilsConnectes()
+{
+    QList<QBluetoothAddress> appareils;
+    appareils = appareilLocal.connectedDevices();
+    qDebug() << "Appareils connectés :" << appareils;
+}
+
+void CommunicationBluetooth::demarrerScanAppareil()
+{
+    QBluetoothDeviceDiscoveryAgent* agentDecouverte =
+      new QBluetoothDeviceDiscoveryAgent(this);
+    connect(agentDecouverte,
+            SIGNAL(deviceDiscovered(QBluetoothDeviceInfo)),
+            this,
+            SLOT(appareilDecouvert(QBluetoothDeviceInfo)));
+
+    agentDecouverte->start();
+}
+
+void CommunicationBluetooth::appareilDecouvert(
+  const QBluetoothDeviceInfo& appareil)
+{
+    qDebug() << "Nouvel appareil trouvé:" << appareil.name() << '('
+             << appareil.address().toString() << ')';
 }
--- quizzy-desktop/communicationbluetooth.h
+++ quizzy-desktop/communicationbluetooth.h
@@ -1,10 +1,26 @@
 #ifndef COMMUNICATIONBLUETOOTH_H
 #define COMMUNICATIONBLUETOOTH_H
 
+/**
+ * @file communicationbluetooth.h
+ *
+ * @brief Déclaration de la classe CommunicationBluetooth
+ * @author Thomas HNIZDO
+ * @version 0.2
+ */
+
 #include <QObject>
 #include <QBluetoothAddress>
 #include <QBluetoothSocket>
+#include <QBluetoothLocalDevice>
+#include <QBluetoothDeviceDiscoveryAgent>
+#include <QBluetoothDeviceInfo>
 
+/**
+ * @class CommunicationBluetooth
+ * @brief Déclaration de la classe CommunicationBluetooth
+ * @details Cette classe gère la connexion bluetooth
+ */
 class CommunicationBluetooth : public QObject
 {
     Q_OBJECT
@@ -14,9 +30,22 @@ class CommunicationBluetooth : public QObject
     QBluetoothAddress adresseAppareil;
     QBluetoothSocket* socket;
 
+    QBluetoothLocalDevice appareilLocal;
+    QString               nomAppareilLocal;
+
+  public slots:
+    void appareilDecouvert(const QBluetoothDeviceInfo& appareil);
+
   public:
     CommunicationBluetooth(QObject* parent = nullptr);
     ~CommunicationBluetooth();
+
+    void verifierBluetooth();
+    void activerBluetooth();
+    void lireNomAppareil();
+    void rendreAppareilVisible();
+    void obtenirAppareilsConnectes();
+    void demarrerScanAppareil();
 };
 
 #endif // COMMUNICATIONBLUETOOTH_H
